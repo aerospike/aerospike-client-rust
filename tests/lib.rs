f88f6f53b6b21847057d2fe1f1f51706903c9e8e
Jan Hecking <jhecking@aerospike.com>
2016-11-28 14:17:29 -0800

    fix indentation

--- tests/lib.rs
+++ tests/lib.rs
@@ -38,39 +38,39 @@ pub mod common1 {
     }
 
     lazy_static! {
-	    pub static ref AEROSPIKE_HOST: String = match env::var("AEROSPIKE_HOST") {
-	        Ok(s) => s,
-	        Err(_) => "127.0.0.1".to_string(),
-	    };
+        pub static ref AEROSPIKE_HOST: String = match env::var("AEROSPIKE_HOST") {
+            Ok(s) => s,
+                Err(_) => "127.0.0.1".to_string(),
+        };
 
-	    pub static ref AEROSPIKE_PORT: u16 = match env::var("AEROSPIKE_PORT") {
-	        Ok(s) => s.parse().unwrap(),
-	        Err(_) => 3000,
-	    };
-	    pub static ref AEROSPIKE_NAMESPACE: String = match env::var("AEROSPIKE_NAMESPACE") {
-	        Ok(s) => s,
-	        Err(_) => "test".to_string(),
-	    };
-	    pub static ref GLOBAL_CLIENT_POLICY: ClientPolicy = {
-	    	let mut cp = ClientPolicy::default();
-	    	match env::var("AEROSPIKE_USER") {
-	    		Ok(user) => {
-	    			let pass =  match env::var("AEROSPIKE_PASSWORD") {
-	    				Ok(pass) => pass,
-	    				Err(_) => "".to_string(),
-	    			};
+        pub static ref AEROSPIKE_PORT: u16 = match env::var("AEROSPIKE_PORT") {
+            Ok(s) => s.parse().unwrap(),
+                Err(_) => 3000,
+        };
+        pub static ref AEROSPIKE_NAMESPACE: String = match env::var("AEROSPIKE_NAMESPACE") {
+            Ok(s) => s,
+                Err(_) => "test".to_string(),
+        };
+        pub static ref GLOBAL_CLIENT_POLICY: ClientPolicy = {
+            let mut cp = ClientPolicy::default();
+            match env::var("AEROSPIKE_USER") {
+                Ok(user) => {
+                    let pass =  match env::var("AEROSPIKE_PASSWORD") {
+                        Ok(pass) => pass,
+                            Err(_) => "".to_string(),
+                    };
 
-	    			cp.set_user_password(Some((user, pass))).unwrap();
-	    		}
-	    		Err(_) => (),
-	    	}
+                    cp.set_user_password(Some((user, pass))).unwrap();
+                }
+                Err(_) => (),
+            }
 
-	    	cp
-	    };
-	    pub static ref GLOBAL_CLIENT: Arc<Client> = {
-	    	Arc::new(Client::new(&GLOBAL_CLIENT_POLICY, &vec![Host::new(&AEROSPIKE_HOST, *AEROSPIKE_PORT)]).unwrap())
-	    };
-	}
+            cp
+        };
+        pub static ref GLOBAL_CLIENT: Arc<Client> = {
+            Arc::new(Client::new(&GLOBAL_CLIENT_POLICY, &vec![Host::new(&AEROSPIKE_HOST, *AEROSPIKE_PORT)]).unwrap())
+        };
+    }
 }
 
 #[macro_use]
