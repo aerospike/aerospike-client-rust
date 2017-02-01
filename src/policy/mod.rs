2fc6e54c949460bb6310fdc273101b429596ac99
Jan Hecking <jhecking@netgaroo.com>
2017-02-01 17:20:37 +0800

    hide policy::*_policy sub-modules

--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -13,18 +13,17 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-pub mod priority;
-pub mod consistency_level;
-pub mod generation_policy;
-pub mod record_exists_action;
-pub mod commit_level;
-
-pub mod client_policy;
-pub mod read_policy;
-pub mod write_policy;
-pub mod scan_policy;
-pub mod query_policy;
-pub mod admin_policy;
+mod priority;
+mod consistency_level;
+mod generation_policy;
+mod record_exists_action;
+mod commit_level;
+mod client_policy;
+mod read_policy;
+mod write_policy;
+mod scan_policy;
+mod query_policy;
+mod admin_policy;
 
 pub use self::priority::Priority;
 pub use self::consistency_level::ConsistencyLevel;
