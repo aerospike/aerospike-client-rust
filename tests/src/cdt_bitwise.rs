7dd8bb1ad1b2537e2cd6645988fd3419e107054a
Jonas Breuer <j.breuer@fz-juelich.de>
2020-08-09 03:55:01 +0200

    Added tests for bitwise operations

--- tests/src/cdt_bitwise.rs
+++ tests/src/cdt_bitwise.rs
@@ -17,8 +17,8 @@ use crate::common;
 use env_logger;
 
 use aerospike::operations::bitwise;
-use aerospike::operations::bitwise::BitPolicy;
-use aerospike::{as_bin, as_key, Value, WritePolicy};
+use aerospike::operations::bitwise::{BitPolicy, CdtBitwiseOverflowActions};
+use aerospike::{as_key, Value, WritePolicy};
 
 #[test]
 fn cdt_bitwise() {
@@ -30,15 +30,92 @@ fn cdt_bitwise() {
 
     let wpolicy = WritePolicy::default();
     let key = as_key!(namespace, set_name, -1);
-    let val = Value::Blob(vec![0b00000101]);
-    let wbin = as_bin!("bin", val.clone());
-    let bins = vec![&wbin];
+    let val = Value::Blob(vec![0b00000001, 0b01000010, 0b00000011, 0b00000100, 0b00000101]);
     let bpolicy = BitPolicy::default();
 
     client.delete(&wpolicy, &key).unwrap();
 
-    client.put(&wpolicy, &key, &bins).unwrap();
-    let ops = &vec![bitwise::insert("bin2", 0, &val ,&bpolicy), bitwise::get("bin2", 0, 8)];
+    // Verify the insert and Get Command
+    let ops = &vec![bitwise::insert("bin", 0, &val ,&bpolicy), bitwise::get("bin", 9, 5)];
     let rec = client.operate(&wpolicy, &key, ops).unwrap();
-    assert_eq!(*rec.bins.get("bin2").unwrap(), Value::Blob(vec![0b00000101]));
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b10000000]));
+
+    // Verify the Count command
+    let ops = &vec![bitwise::count("bin", 20, 4)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Int(2));
+
+    // Verify the set command
+    let val = Value::Blob(vec![0b11100000]);
+    let ops = &vec![bitwise::set("bin", 13, 3, &val, &bpolicy), bitwise::get("bin", 0, 40)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b00000001, 0b01000111, 0b00000011, 0b00000100, 0b00000101]));
+
+    // Verify Remove command
+    let ops = &vec![bitwise::remove("bin", 0, 1, &bpolicy), bitwise::get("bin", 0, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01000111]));
+
+    // Verify OR command
+    let val = Value::Blob(vec![0b10101010]);
+    let ops = &vec![bitwise::or("bin", 0, 8, &val, &bpolicy), bitwise::get("bin", 0, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b11101111]));
+
+    // Verify XOR command
+    let val = Value::Blob(vec![0b10101100]);
+    let ops = &vec![bitwise::xor("bin", 0, 8, &val, &bpolicy), bitwise::get("bin", 0, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01000011]));
+
+    // Verify AND command
+    let val = Value::Blob(vec![0b01011010]);
+    let ops = &vec![bitwise::and("bin", 0, 8, &val, &bpolicy), bitwise::get("bin", 0, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01000010]));
+
+    // Verify NOT command
+    let ops = &vec![bitwise::not("bin", 0, 8, &bpolicy), bitwise::get("bin", 0, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b10111101]));
+
+    // Verify LSHIFT command
+    let ops = &vec![bitwise::lshift("bin", 24, 8, 3, &bpolicy), bitwise::get("bin", 24, 8)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b00101000]));
+
+    // Verify RSHIFT command
+    let ops = &vec![bitwise::rshift("bin", 0, 9, 1, &bpolicy), bitwise::get("bin", 0, 16)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01011110, 0b10000011]));
+
+    // Verify Add command
+    let ops = &vec![bitwise::add("bin", 0, 8, 128,false,CdtBitwiseOverflowActions::Fail, &bpolicy), bitwise::get("bin", 0, 32)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b11011110, 0b10000011, 0b00000100, 0b00101000]));
+
+    // Verify Subtract command
+    let ops = &vec![bitwise::subtract("bin", 0, 8, 128,false,CdtBitwiseOverflowActions::Fail, &bpolicy), bitwise::get("bin", 0, 32)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01011110, 0b10000011, 0b00000100, 0b00101000]));
+
+    // Verify the set int command
+    let ops = &vec![bitwise::set_int("bin", 8, 8, 255, &bpolicy), bitwise::get("bin", 0, 32)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Blob(vec![0b01011110, 0b11111111, 0b00000100, 0b00101000]));
+
+    // Verify the get int command
+    let ops = &vec![bitwise::get_int("bin", 8, 8, false)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Int(255));
+
+    // Verify the LSCAN command
+    let ops = &vec![bitwise::lscan("bin", 19, 8, true)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Int(2));
+
+    // Verify the RSCAN command
+    let ops = &vec![bitwise::rscan("bin", 19, 8, true)];
+    let rec = client.operate(&wpolicy, &key, ops).unwrap();
+    assert_eq!(*rec.bins.get("bin").unwrap(), Value::Int(7));
 }
