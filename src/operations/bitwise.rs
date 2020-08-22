1c998283d6d3ac1485028bfc7f6a6830ce948413
Jonas Breuer <j.breuer@fz-juelich.de>
2020-08-22 19:08:45 +0200

    Fixed comment on bitwise.rs

--- src/operations/bitwise.rs
+++ src/operations/bitwise.rs
@@ -5,10 +5,11 @@
 //!
 //! Nested CDT operations are supported by optional CTX context arguments.  Example:
 //! ```
-//! bin = [[0b00000001, 0b01000010], [0b01011010]]
-//! Resize first bitmap (in a list of bitmaps) to 3 bytes.
-//! BitOperation.resize("bin", 3, BitResizeFlags.DEFAULT, CTX.listIndex(0))
-//! bin result = [[0b00000001, 0b01000010, 0b00000000], [0b01011010]]
+//! use aerospike::operations::bitwise::{resize, BitwiseResizeFlags, BitPolicy};
+//! // bin = [[0b00000001, 0b01000010], [0b01011010]]
+//! // Resize first bitmap (in a list of bitmaps) to 3 bytes.
+//! resize("bin", 3, Some(BitwiseResizeFlags::Default as u8), &BitPolicy::default());
+//! // bin result = [[0b00000001, 0b01000010, 0b00000000], [0b01011010]]
 //! ```
 //!
 
