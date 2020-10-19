9982a3ec66ac5c2990b24aea943dd1304bdf1804
Jonas Breuer <j.breuer@fz-juelich.de>
2020-10-19 22:42:44 +0200

    Expression tests

--- tests/src/exp.rs
+++ tests/src/exp.rs
@@ -15,11 +15,12 @@
 use crate::common;
 use env_logger;
 
-use aerospike::exp::Expression;
+use aerospike::exp::{ExpType, Expression, FilterExpression};
 use aerospike::Task;
 use aerospike::*;
+use std::sync::Arc;
 
-const EXPECTED: usize = 1000;
+const EXPECTED: usize = 100;
 
 fn create_test_set(no_records: usize) -> String {
     let client = common::client();
@@ -29,8 +30,14 @@ fn create_test_set(no_records: usize) -> String {
     let wpolicy = WritePolicy::default();
     for i in 0..no_records as i64 {
         let key = as_key!(namespace, &set_name, i);
-        let wbin = as_bin!("bin", i);
-        let bins = vec![&wbin];
+        let ibin = as_bin!("bin", i);
+        let sbin = as_bin!("bin2", format!("{}", i));
+        let fbin = as_bin!("bin3", i as f64 / 3 as f64);
+        let str = format!("{}{}", "blob", i);
+        let bbin = as_bin!("bin4", str.as_bytes());
+        let lbin = as_bin!("bin5", as_list!("a", "b", i));
+        let mbin = as_bin!("bin6", as_map!("a" => "test", "b" => i));
+        let bins = vec![&ibin, &sbin, &fbin, &bbin, &lbin, &mbin];
         client.delete(&wpolicy, &key).unwrap();
         client.put(&wpolicy, &key, &bins).unwrap();
     }
@@ -51,30 +58,311 @@ fn create_test_set(no_records: usize) -> String {
 }
 
 #[test]
-fn query_exp() {
+fn expression_compare() {
     let _ = env_logger::try_init();
 
+    let set_name = create_test_set(EXPECTED);
+
+    // EQ
+    let rs = test_filter(
+        Expression::eq(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "EQ Test Failed");
+
+    // NE
+    let rs = test_filter(
+        Expression::ne(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 99, "NE Test Failed");
+
+    // LT
+    let rs = test_filter(
+        Expression::lt(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(10),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 10, "LT Test Failed");
+
+    // LE
+    let rs = test_filter(
+        Expression::le(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(10),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 11, "LE Test Failed");
+
+    // GT
+    let rs = test_filter(
+        Expression::gt(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 98, "GT Test Failed");
+
+    // GE
+    let rs = test_filter(
+        Expression::ge(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 99, "GT Test Failed");
+}
+
+#[test]
+fn expression_condition() {
+    let _ = env_logger::try_init();
+
+    let set_name = create_test_set(EXPECTED);
+
+    // AND
+    let rs = test_filter(
+        Expression::and(vec![
+            Expression::eq(
+                Expression::int_bin("bin".to_string()),
+                Expression::int_val(1),
+            ),
+            Expression::eq(
+                Expression::string_bin("bin2".to_string()),
+                Expression::string_val("1".to_string()),
+            ),
+        ]),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "AND Test Failed");
+
+    // OR
+    let rs = test_filter(
+        Expression::or(vec![
+            Expression::eq(
+                Expression::int_bin("bin".to_string()),
+                Expression::int_val(1),
+            ),
+            Expression::eq(
+                Expression::int_bin("bin".to_string()),
+                Expression::int_val(3),
+            ),
+        ]),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 2, "OR Test Failed");
+
+    // NOT
+    let rs = test_filter(
+        Expression::not(Expression::eq(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        )),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 99, "NOT Test Failed");
+}
+
+#[test]
+fn expression_data_types() {
+    let _ = env_logger::try_init();
+
+    let set_name = create_test_set(EXPECTED);
+
+    // INT
+    let rs = test_filter(
+        Expression::eq(
+            Expression::int_bin("bin".to_string()),
+            Expression::int_val(1),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "INT Test Failed");
+
+    // STRING
+    let rs = test_filter(
+        Expression::eq(
+            Expression::string_bin("bin2".to_string()),
+            Expression::string_val("1".to_string()),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "STRING Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(
+            Expression::float_bin("bin3".to_string()),
+            Expression::float_val(2f64),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "FLOAT Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(
+            Expression::blob_bin("bin4".to_string()),
+            Expression::blob_val(format!("{}{}", "blob", 5).into_bytes()),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "BLOB Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(
+            Expression::bin("bin4".to_string(), ExpType::BLOB),
+            Expression::blob_val(format!("{}{}", "blob", 5).into_bytes()),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 1, "BLOB BIN Test Failed");
+
+    let rs = test_filter(
+        Expression::ne(
+            Expression::bin_type("bin".to_string()),
+            Expression::int_val(0),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "BIN TYPE Test Failed");
+}
+
+#[test]
+fn expression_rec_ops() {
+    let _ = env_logger::try_init();
+
+    let set_name = create_test_set(EXPECTED);
+
+    // dev size 0 because inmemory
+    let rs = test_filter(
+        Expression::gt(Expression::device_size(), Expression::int_val(0)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "DEVICE SIZE Test Failed");
+
+    let rs = test_filter(
+        Expression::gt(Expression::last_update(), Expression::int_val(15000)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "LAST UPDATE Test Failed");
+
+    let rs = test_filter(
+        Expression::gt(Expression::since_update(), Expression::int_val(150)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "SINCE UPDATE Test Failed");
+
+    // Records dont expire
+    let rs = test_filter(
+        Expression::gt(Expression::void_time(), Expression::int_val(1500000000)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "VOID TIME Test Failed");
+
+    let rs = test_filter(
+        Expression::gt(Expression::ttl(), Expression::int_val(0)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "TTL Test Failed");
+
+    let rs = test_filter(Expression::not(Expression::is_tombstone()), &set_name);
+    let count = count_results(rs);
+    assert_eq!(count, 100, "TOMBSTONE Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(
+            Expression::set_name(),
+            Expression::string_val(set_name.clone()),
+        ),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count, 100, "SET NAME Test Failed");
+
+    let rs = test_filter(Expression::bin_exists("bin4".to_string()), &set_name);
+    let count = count_results(rs);
+    assert_eq!(count, 100, "BIN EXISTS Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(Expression::digest_modulo(3), Expression::int_val(1)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    assert_eq!(count > 0 && count < 100, true, "DIGEST MODULO Test Failed");
+
+    let rs = test_filter(
+        Expression::eq(Expression::key(ExpType::INT), Expression::int_val(50)),
+        &set_name,
+    );
+    let count = count_results(rs);
+    // 0 because key is not saved
+    assert_eq!(count, 0, "KEY Test Failed");
+
+    let rs = test_filter(Expression::key_exists(), &set_name);
+    let count = count_results(rs);
+    // 0 because key is not saved
+    assert_eq!(count, 0, "KEY EXISTS Test Failed");
+
+    /* let rs = test_filter(
+        Expression::eq(Expression::nil(), Expression::nil()),
+        &set_name,
+    );
+    let count = count_results(rs);
+    // 0 because key is not saved
+    assert_eq!(count, 0, "NIL Test Failed"); */
+}
+
+fn test_filter(filter: FilterExpression, set_name: &str) -> Arc<Recordset> {
     let client = common::client();
     let namespace = common::namespace();
-    let set_name = create_test_set(EXPECTED);
+
     let mut qpolicy = QueryPolicy::default();
-    qpolicy.filter_expression = Some(Expression::eq(
-        Expression::int_bin("bin".to_string()),
-        Expression::int_val(1),
-    ));
-    // Filter Query
-    let statement = Statement::new(namespace, &set_name, Bins::All);
-    let rs = client.query(&qpolicy, statement).unwrap();
+    qpolicy.filter_expression = Some(filter);
+
+    let statement = Statement::new(namespace, set_name, Bins::All);
+    client.query(&qpolicy, statement).unwrap()
+}
+
+fn count_results(rs: Arc<Recordset>) -> usize {
     let mut count = 0;
 
     for res in &*rs {
         match res {
-            Ok(rec) => {
-                assert_eq!(rec.bins["bin"], as_val!(1));
+            Ok(_) => {
                 count += 1;
             }
             Err(err) => panic!(format!("{:?}", err)),
         }
     }
-    assert_eq!(count, 1);
+
+    count
 }
