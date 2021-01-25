c644c38d06744a486721921fa3e6e0511fc416e6
Jonas Breuer <j.breuer@fz-juelich.de>
2021-01-25 18:07:23 +0100

    Fixed device Size test for not in-memory namespaces (#91)

--- tests/src/exp.rs
+++ tests/src/exp.rs
@@ -240,7 +240,16 @@ fn expression_rec_ops() {
         le(device_size(), int_val(0)),
         &set_name,
     );
-    let count = count_results(rs);
+    let mut count = count_results(rs);
+
+    if count == 0 {
+        // Not in-memory
+        let rs = test_filter(
+            le(device_size(), int_val(2000)),
+            &set_name,
+        );
+        count = count_results(rs);
+    }
     assert_eq!(count, 100, "DEVICE SIZE Test Failed");
 
     let rs = test_filter(
