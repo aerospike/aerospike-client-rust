e5ecb23b6fe9c0c7af452fd46881a7c8db94b2ad
yrizhkov <yrizhkov@aerospike.com>
2025-07-10 21:15:05 +0300

    Apply max_concurrent_nodes policy in query and scan operations

--- aerospike-rt/src/lib.rs
+++ aerospike-rt/src/lib.rs
@@ -6,10 +6,10 @@ compile_error!("Please select only one runtime");
 
 #[cfg(all(any(feature = "rt-async-std"), not(feature = "rt-tokio")))]
 pub use async_std::{
-    self, fs, future::timeout, io, net, sync::RwLock, task, task::sleep, task::spawn,
+    self, fs, future::timeout, io, net, sync::RwLock, sync::Semaphore, task, task::sleep, task::spawn,
 };
 #[cfg(all(any(feature = "rt-tokio"), not(feature = "rt-async-std")))]
-pub use tokio::{self, fs, io, net, spawn, sync::RwLock, task, time, time::sleep, time::timeout};
+pub use tokio::{self, fs, io, net, spawn, sync::RwLock, sync::Semaphore, task, time, time::sleep, time::timeout};
 
 #[cfg(all(any(feature = "rt-async-std"), not(feature = "rt-tokio")))]
 pub use std::time;
