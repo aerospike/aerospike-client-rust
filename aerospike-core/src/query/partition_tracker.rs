901e42de7c54177a05d969cd7786b4431627f207
Khosrow Afroozeh <parshua@gmail.com>
2025-10-20 17:09:59 +0200

    Fix minor comment typos

--- aerospike-core/src/query/partition_tracker.rs
+++ aerospike-core/src/query/partition_tracker.rs
@@ -3,7 +3,7 @@
 // Portions may be licensed to Aerospike, Inc. under one or more contributor
 // license agreements.
 //
-// Licensed under the Apache Licenseersion 2.0 (the "License"); you may not
+// Licensed under the Apache License version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
 // the License at http://www.apache.org/licenses/LICENSE-2.0
 //
