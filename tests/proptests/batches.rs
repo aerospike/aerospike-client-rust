c1f4da62e62c3c14adba9b4a873bf1dc95428798
Khosrow Afroozeh <parshua@gmail.com>
2025-12-03 20:44:59 +0100

    WIP debugged version of the proptests for Sam

--- tests/proptests/batches.rs
+++ tests/proptests/batches.rs
@@ -15,56 +15,68 @@ use crate::proptests::{
     batch_operation::*, bins::*, key::*, operation::*, partition_filter::*, policy::*,
 };
 
+prop_compose! {
+    pub fn bop_read_bins1()(
+        _brp in batch_read_policy(),
+    ) -> PropBatchOperation {
+        PropBatchOperation::ReadBins(BatchReadPolicy::default(), Bins::All)
+    }
+}
+
 proptest_async::proptest! {
     #[test]
     async fn batch_read(
         i in 0..10,
-        batch_policy in batch_policy(30000),
-        ops in many_batch_read_operations(8, false),
-		write_mask in 1..256,
+        _batch_policy in batch_policy(30000),
+        ops in bop_read_bins1(),
     ) {
         let client = common::singleton_client().await;
         let namespace: &str = common::namespace();
         let set_name: &str = common::prop_setname();
 
+        println!("{}",i);
+        let key = as_key!(namespace, set_name, i);
+
         // let as_ops: Vec<aerospike::operations::Operation> = ops.into_iter().map(|op| op.to_op()).collect();
         let mut as_ops = vec![];
-		let mut bit_mask = 1;
-        for op in &ops {
-            let key = as_key!(namespace, set_name, i);
-            let mut as_op = op.to_op(key.clone());
+        // for op in &ops {
+            let as_op = ops.to_op(key.clone());
 
-			// Not all reads will have a valid key that leads to content.
-			// Does this read have a corresponding write to initialize a bin?
+            // Not all reads will have a valid key that leads to content.
+            // Does this read have a corresponding write to initialize a bin?
 
-			if (bit_mask & write_mask) != 0 {
-				let bin = as_bin!("binName", "binValue");
-				let bins = [bin.clone()];
-				let mut write_policy = WritePolicy::new(0, Expiration::Seconds(60));
+            if i % 2 == 0 {
+                let bin = as_bin!("binName", "binValue");
+                let bins = [bin];
+                let write_policy = WritePolicy::default();
 
-				// TO ensure maximum chance of successfully reading from the record,
-				// we set filter expressions to None.  Here, we set it to None for
-				// the Put operation explicitly.  For the reads, we rely on the
-				// "false" parameter of many_batch_read_operations(), elsewhere.
-				// The idea is that if both read and write policies have equal
-				// filter expressions (namely, none at all), there should be no
-				// reason for a filter operation to exclude any records.
+                // TO ensure maximum chance of successfully reading from the record,
+                // we set filter expressions to None.  Here, we set it to None for
+                // the Put operation explicitly.  For the reads, we rely on the
+                // "false" parameter of many_batch_read_operations(), elsewhere.
+                // The idea is that if both read and write policies have equal
+                // filter expressions (namely, none at all), there should be no
+                // reason for a filter operation to exclude any records.
 
-				write_policy.base_policy.filter_expression = None;
+                // write_policy.base_policy.filter_expression = None;
 
-				// SAFETY: This is just a test, not production code.
-				// Use of unwrap() here is OK; if something goes wrong, we
-				// WANT the Rust runtime to panic.
+                // SAFETY: This is just a test, not production code.
+                // Use of unwrap() here is OK; if something goes wrong, we
+                // WANT the Rust runtime to panic.
 
-				client.put(&write_policy, &key, &bins).await.unwrap();
-			}
+                println!("WRITING: {}", i);
+                client.put(&write_policy, &key, &bins).await.unwrap();
+                let res = client.get(&ReadPolicy::default(), &key, Bins::All).await.unwrap();
+                println!("THE RECORD IS: {:?}", res);
+            }
 
             as_ops.push(as_op);
+        // }
 
-			bit_mask = bit_mask << 1;
-        }
-
+        println!("{:?}", ops);
+        let batch_policy = BatchPolicy::default();
         let res = client.batch(&batch_policy, &as_ops).await;
+
 
         match res {
         //     Err(Error::ServerError(ResultCode::ParameterError, _, _)) => {
@@ -86,17 +98,18 @@ proptest_async::proptest! {
         //         panic!("{}", e);
         //     },
             Err(e @ Error::BatchError(index, ResultCode::BinTypeError, _, _)) => {
-				panic!("Batch result ERROR: index={} {:?}", index, e)
-			}
+                panic!("Batch result ERROR: index={} {:?}", index, e)
+            }
             Err(e) => panic!("{}", e),
-            Ok(res) => (), //println!("OK"),
+            Ok(res) => {
+                // Data validation
+                for op in res {
+                    eprintln!("OP: {:?}", op.record.map(|r| r.bins.len()));
+                }
+            }, //println!("OK"),
         }
 
-		// Data validation
-		for op in as_ops {
-			eprintln!("OP: {:?}", op);
-		}
-		eprintln!("--------------------");
+        eprintln!("--------------------");
     }
 
     #[test]
