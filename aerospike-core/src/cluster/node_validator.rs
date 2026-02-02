a9334c217bc4bd5591cd2887cca3bc1588fde0ff
Dheeraj Mishra <dmishra@aerospike.com>
2026-02-02 19:20:39 +0530

    CLIENT-4113: added policies to access host when ip are not discovered implicitly, ensuring RU and I workload are functional

--- aerospike-core/src/cluster/node_validator.rs
+++ aerospike-core/src/cluster/node_validator.rs
@@ -151,6 +151,7 @@ impl NodeValidator {
     }
 
     fn set_services(&mut self, alias: &Host, peers: &str) {
+        let ip_map = self.client_policy.ip_map.clone();
         let peers = peers.split(';');
         for peer in peers {
             match peer.to_hosts() {
@@ -158,9 +159,14 @@ impl NodeValidator {
                 Ok(host) => {
                     let mut host: Vec<Host> = host
                         .into_iter()
-                        .map(|h| Host {
-                            tls_name: alias.tls_name.clone(),
-                            ..h
+                        .map(|mut h| {
+                           h.tls_name = alias.tls_name.clone();
+                           if let Some(ref ip_map) = ip_map {
+                              if let Some(mapped) = ip_map.get(&h.name) {
+                                 h.name = mapped.clone();
+                              }
+                           }
+                           h
                         })
                         .collect();
                     self.services.append(&mut host);
